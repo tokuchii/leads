aa8ecb48b0331d59cb044721dfad31495d9c0591
tokuchii <tokuchii081@gmail.com>
2025-06-05 19:02:48 +0800

    Initial Laravel + Vue setup

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+createApp(ExampleComponent).mount('#app');
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>Hello from Vue!</h2>
+</template>
+
+<script setup>
+// You can add Vue logic here
+</script> 
