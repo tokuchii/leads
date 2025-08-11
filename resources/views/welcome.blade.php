05592536ca0fb12fe6974776ae3085acf61df12e
tokuchii <tokuchii081@gmail.com>
2025-08-11 14:41:26 +0800

    minimal changes/logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>LeadsAgri</title>
+    <link rel="icon" type="image/png" href="{{ asset('/images/logo-green.png') }}">
 
     <!-- Styles -->
     <style>
